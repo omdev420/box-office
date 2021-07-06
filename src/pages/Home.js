b7bc64ac15e67d39bcb258e3d286051eb98d755c
omdev420 <omdev420@gmail.com>
2021-07-06 13:05:10 +0530

    created input and fetch TVMAZE api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line import/no-cycle
 import { MainPageLayout } from '../components/MainPageLayout';
 
 export const Home = () => {
-  return <MainPageLayout>THIS IS HOME</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r =>
+      r.json().then(results => {
+        console.log(results);
+      })
+    );
+  };
+
+  const onKeyDown = ev => {
+    const enterPressed = ev.keyCode === 13;
+    if (enterPressed) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        val={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
