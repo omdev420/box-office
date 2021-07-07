e627fc650c9e0675bd6995e6cce805a0caa7376d
omdev420 <omdev420@gmail.com>
2021-07-07 13:03:29 +0530

    added grids and cards for the results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 // eslint-disable-next-line import/no-cycle
 import { MainPageLayout } from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 
 export const Home = () => {
   const [input, setInput] = useState('');
@@ -31,11 +33,11 @@ export const Home = () => {
       return <div>No results</div>;
     }
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
